6b24c4f7f27732c2449cdd78e1648eb6b37c2259
Mosketxu <mosketxu@gmail.com>
2019-12-18 14:08:04 +0100

    navbar y logout

--- resources/views/campaign/_navbarcampaign.blade.php
+++ resources/views/campaign/_navbarcampaign.blade.php
@@ -1,7 +1,7 @@
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
   <!-- Left navbar links -->
-  <ul class="navbar-nav">
+  <ul class="navbar-nav  mr-auto">
     <li class="nav-item">
       <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
     </li>
@@ -30,7 +30,38 @@
         <a  href="{{route('campaign.etiquetas.index',$campaign->id) }}" class="nav-link" title="Etiquetas HTML"><span id="navetiquetas" class="px-1"><i class="fas fa-code text-indigo fa-lg"></i></span></a>
     </li>
   </ul>
+  <!-- Right Side Of Navbar --> 
+  <ul class="navbar-nav ml-auto">
+    <!-- Authentication Links -->
+    @guest
+        <li class="nav-item">
+            {{-- <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a> --}}
+        </li>
+        @if (Route::has('register'))
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+        @endif
+    @else
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
 
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
+        </li>
+      @endguest
+  </ul>
   
 </nav>
 <!-- /.navbar -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,29 +21,29 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm"> --}}
+            {{-- <div class="container"> --}}
+                {{-- <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                </a> --}}
+                {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> --}}
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent"> --}}
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    {{-- <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                    </ul> --}}
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <!-- Right Side Of Navbar --> 
+                    {{-- <ul class="navbar-nav ml-auto"> --}}
                         <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
+                        {{-- @guest
+                            <li class="nav-item"> --}}
                                 {{-- <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a> --}}
-                            </li>
-                            @if (Route::has('register'))
+                            {{-- </li> --}}
+                            {{-- @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
@@ -66,11 +66,11 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                        @endguest --}}
+                    {{-- </ul> --}}
+                {{-- </div>
+            </div> --}}
+        {{-- </nav> --}}
 
         <main class="py-4">
             @yield('content')
